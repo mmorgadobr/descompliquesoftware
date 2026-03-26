d92ba5ea18f1c351f9941483953b4e00b4a81508
Michel Morgado <michelmk13@gmail.com>
2026-03-25 22:15:39 -0300

    ajuste route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -4,11 +4,14 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: true, // true para porta 465
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 export async function POST(req: Request) {
